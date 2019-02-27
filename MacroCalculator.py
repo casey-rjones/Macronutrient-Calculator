eb3ea0883dab705082fcd80eab5021a67fcfd20b
Casey <47370829+casey-rjones@users.noreply.github.com>
2019-02-26 19:54:28 -0600

    Update MacroCalculator.py

--- MacroCalculator.py
+++ MacroCalculator.py
@@ -1,84 +1,35 @@
-import sys
-from PyQt5.QtWidgets import *
-#from PyQt5 import QtGui
+# This function adds two numbers 
+def maintCalc(x, y):
+   return (x * 10) * y
 
-app = QApplication(sys.argv)
-widget = QWidget()
-label = QLabel("", widget)
+print("Select operation.")
+print("1.Calculate Body Weight Maintenance")
+print("2.Calculate Macros")
 
-btnAdd = QPushButton("Add", widget)
-btnSub = QPushButton("Subtract", widget)
-btnDiv = QPushButton("Divide", widget)
-btnMul = QPushButton("Multiply", widget)
+# Take input from the user 
+choice = input("Enter choice(1 or 2): ")
 
 
-#txtArea = QPlainTextEdit("Text To Edit", widget)widget.resize
-
-txtBodyWeight = QLineEdit("Body Weight (in LBS)", widget)
-txtActivityLevel = QLineEdit("Activity Level", widget)
-
-def init():
-
-    widget.resize(300, 300)
-    widget.move(300, 300)
-    widget.setWindowTitle('Macro Calculator')
-    widget.show()
-
-    txtBodyWeight.move(90,10)
-    txtBodyWeight.show()
-    txtActivityLevel.move(90,60)
-    txtActivityLevel.show()
-
-    label.setText("")
-    label.move(20,110)
-    label.show()
-
-    btnAdd.setToolTip('Addition')
-    btnAdd.move(20,160)
-    btnAdd.clicked.connect(addition)
-    btnAdd.show()
-
-    btnSub.setToolTip('Subtraction')
-    btnSub.move(110,160)
-    btnSub.clicked.connect(subtraction)
-    btnSub.show()
-
-    btnDiv.setToolTip('Division')
-    btnDiv.move(20,210)
-    btnDiv.clicked.connect(division)
-    btnDiv.show()
-
-    btnMul.setToolTip('Multiplication')
-    btnMul.move(110,210)
-    btnMul.clicked.connect(multiplication)
-    btnMul.show()
+ 
 
 
-def addition():
-    num1 = int(txtBodyWeight.text())
-    num2 = int(txtActivityLevel.text())
-    label.setFixedWidth(200)
-    label.setText("Addition: "+str(num1 + num2))
 
-def subtraction():
-    num1 = int(txtBodyWeight.text())
-    num2 = int(txtActivityLevel.text())
-    label.setFixedWidth(200)
-    label.setText("Subtraction: "+str(num1 - num2))
 
-def multiplication():
-    num1 = int(txtBodyWeight.text())
-    num2 = int(txtActivityLevel.text())
-    label.setFixedWidth(200)
-    label.setText("Multiplication: "+str(num1 * num2))
+if choice == '1':
+   bodyWeight = int(input("Enter Body Weight (lbs): "))
+   gender = (input("Enter Gender (M or F): "))
+   height = int(input("Enter Height (inches): "))
+   age = int(input("Enter Age: "))
+   activityLevel = int(input("Enter Activity Level: "))
+   print("Maintenance Calories:", maintCalc(bodyWeight,activityLevel))
+elif choice == '2':
+   bodyWeight = int(input("Enter Body Weight (lbs): "))
+   gender = (input("Enter Gender (M or F): "))
+   height = int(input("Enter Height (inches): "))
+   age = int(input("Enter Age: "))
+   activityLevel = int(input("Enter Activity Level: "))   
+   print(num1,"-",num2,"=", subtract(num1,num2))
 
-def division():
-    num1 = int(txtBodyWeight.text())
-    num2 = int(txtActivityLevel.text())
-    label.setFixedWidth(200)
-    label.setText("Division: "+str(num1 / num2))
+else:
+   print("Invalid input")
 
-if __name__ == "__main__":
-     init()
-
-app.exec_()
